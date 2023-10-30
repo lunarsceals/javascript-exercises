36dc41ea74958a8e27e896c35191ad9accdb793e
lunarsceals <lunarsceals@gmail.com>
2023-10-30 13:20:01 -0400

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let sum = 0;
+	for (let i=0;i<list.length;i++){
+    sum += list[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let product = 1;
+  for (let i=0;i<list.length;i++){
+    product *= list[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n, pow) {
+	return Math.pow(n,pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0){
+    return 1;
+  } else {
+    return n = n * factorial(n - 1);
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +44,4 @@ module.exports = {
   power,
   factorial
 };
+
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let p2 = string.length 
+    let truthValues = []
+    for (let p1 = 0; p1<p2; p1++){
+        if (string[p1] == string[p2]){
+            truthValues.push(true);
+            p2--;
+        }
+    return truthValues.every();
+    }
 };
 
 // Do not edit below this line
